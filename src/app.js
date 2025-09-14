faa54c2b8da80c3f428af5ba5a3baf21bb30b805
learnaslapp-hue <learnaslapp@gmail.com>
2025-09-14 13:30:57 +0800

    updates

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// src/app.js
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -17,9 +18,20 @@ app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-// Swagger
+// --- Swagger (MOUNT AT /swagger) ---
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// 1) Serve static assets from swagger-ui-dist at /swagger/*
+app.use(
+  '/swagger',
+  swaggerUi.serveFiles(swaggerSpec, { explorer: true })
+);
+
+// 2) Serve the HTML that bootstraps Swagger UI at /swagger
+app.get(
+  '/swagger',
+  swaggerUi.setup(swaggerSpec, { explorer: true })
+);
 
 // Health
 app.get('/health', (_req, res) => {
@@ -29,7 +41,7 @@ app.get('/health', (_req, res) => {
 // API routes
 app.use('/api', routes);
 
-// 404 + errors
+// 404 + errors (keep last)
 app.use(notFound);
 app.use(errorHandler);
 
